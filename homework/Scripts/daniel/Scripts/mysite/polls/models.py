825596271517976df5cdd323d563bda8d1c92b4c
noscience <no_science@yahoo.com.tw>
2017-05-04 12:37:14 +0800

    222

---
+++ homework/Scripts/daniel/Scripts/mysite/polls/models.py
@@ -0,0 +1,44 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+from django.utils.encoding import python_2_unicode_compatible
+
+
+@python_2_unicode_compatible  # only if you need to support Python 2
+class Question(models.Model):
+    # ...
+    def __str__(self):
+        return self.question_text
+    # ...
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+
+@python_2_unicode_compatible  # only if you need to support Python 2
+class Choice(models.Model):
+    # ...
+    def __str__(self):
+        return self.choice_text
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+
+
+@python_2_unicode_compatible  # only if you need to support Python 2
+class student(models.Model):
+    # ...
+    def __str__(self):
+        return self.student_id
+
+    student_id = models.CharField(max_length=200)
+    student_name = models.CharField(max_length=200)
+    student_birthday =  models.DateTimeField('date published')
+    student_hobby = models.CharField(max_length=200)        
+
